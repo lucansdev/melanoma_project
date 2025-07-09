6d589dc2e2f4ec38fc1bf37eb316039cdc94794d
Raul Cabral <raulcabralc@gmail.com>
2025-07-09 19:24:02 -0300

    Finished result page (PC)

---
+++ static/js/result.js
@@ -0,0 +1,13 @@
+const resultHTML = document.querySelector("result");
+const accuracyHTML = document.querySelector("accuracy");
+
+const result = localStorage.getItem("result");
+const accuracy = localStorage.getItem("accuracy");
+
+if (result >= 0.5) {
+  resultHTML.innerHTML = "positivo";
+} else {
+  resultHTML.innerHTML = "negativo";
+}
+
+accuracyHTML = accuracy;
