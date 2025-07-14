222076ea47ef625ff78859a11cdfdb7c5a90a9df
lucansdev <146260839+lucansdev@users.noreply.github.com>
2025-07-14 16:48:50 -0300

    Update system.py

--- system.py
+++ system.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from flask import Flask, jsonify, render_template, request,Response
+from flask import Flask, jsonify, render_template, request, Response
 import cv2
 import tensorflow as tf
 import keras
@@ -9,43 +9,36 @@ import os
 app = Flask(__name__)
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "my_model", "0001")
+
 class Image(ABC):
     def __init__(self, directory_image):
         self.directory_image = directory_image
-
     @abstractmethod
     def get_image(self):
-        """
-        This method should be implemented by subclasses to return an image tensor.
-        """
         pass
 
 class LoadImage(Image):
     def __init__(self, directory_image) -> None:
         super().__init__(directory_image)
-
     def get_image(self) -> tf.Tensor:
         image_stream = self.directory_image.read()
         image_array = np.frombuffer(image_stream, np.uint8)
         image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-        
         if image is None:
             raise FileNotFoundError("Image could not be decoded. Please check the file format.")
-        
         image_tensor = tf.convert_to_tensor(image, dtype=tf.float32) / 255.0
         image_tensor = tf.image.resize(image_tensor, (224, 224))
         image_tensor = tf.expand_dims(image_tensor, axis=0)
-
         return image_tensor
 
 class Model(ABC):
     def __init__(self):
         self.model = None
-
     @abstractmethod
     def get_model(self, model_path) -> None:
         pass
-
     @abstractmethod
     def predict(self, image_tensor):
         pass
@@ -53,7 +46,6 @@ class Model(ABC):
 class JeramyModel(Model):
     def __init__(self):
         super().__init__()
-
     def get_model(self, model_path) -> None:
         if not os.path.exists(model_path):
              raise FileNotFoundError(f"Model not found at the specified path: {model_path}")
@@ -63,24 +55,17 @@ class JeramyModel(Model):
             print("--- Model Loaded and Compiled Successfully ---") 
         except Exception as e:
             raise IOError(f"Error loading the model: {e}")
-
     def predict(self, image_tensor):
         if not isinstance(image_tensor, tf.Tensor):
             raise TypeError("Image is not a tensor")
-        
         if self.model is None:
             raise ValueError("Model is not loaded")
-
-        
         model_loaded = self.model.signatures["serving_default"]
         prediction = model_loaded(image_tensor)
         return np.asarray(prediction["output_0"]).tolist()
-    
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(BASE_DIR, "my_model", "0001")
 melanoma_model = JeramyModel()
-melanoma_model.get_model(model_path)
+
 
 
 @app.route("/")
@@ -92,7 +77,6 @@ def predict_cancer():
     if request.method == "GET":
         return render_template("test.html")
     
-
     if request.method == "POST":
         if 'file' not in request.files:
             return jsonify({"error": "No file part in the request"}), 400
@@ -103,6 +87,10 @@ def predict_cancer():
             return jsonify({"error": "No selected file"}), 400
 
         try:
+            if melanoma_model.model is None:
+                print("--- Model not loaded. Loading now... ---")
+                melanoma_model.get_model(model_path)
+
             image_loader = LoadImage(file)
             image_tensor = image_loader.get_image()
             prediction = melanoma_model.predict(image_tensor)
@@ -110,9 +98,9 @@ def predict_cancer():
             if prediction[0][0] >= 0.5:
                 acc = prediction[0][0] * 100
             else:
-                acc = (1- prediction[0][0]) * 100
+                acc = (1 - prediction[0][0]) * 100
 
-            return jsonify({"response": prediction,"acc":acc})
+            return jsonify({"response": prediction, "acc": acc})
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
